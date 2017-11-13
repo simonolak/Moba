b5fe449bc308b384bff32dfcd0c7d2d336398b33
olak-PC\olak <simonolak@163.com>
2017-11-14 00:43:44 +0800

    the first version

---
+++ code/installdebug.bat
@@ -0,0 +1,5 @@
+rem call closefiles
+copy debug\quake3.exe "E:\Games\Quake 3 Arena"
+copy debug\qagamex86.dll "E:\Games\Quake 3 Arena"
+copy debug\cgamex86.dll "E:\Games\Quake 3 Arena"
+copy debug\uix86.dll "E:\Games\Quake 3 Arena"
